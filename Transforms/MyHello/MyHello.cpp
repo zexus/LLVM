3ca620df448f7beffc6051ff072a76a063c7f584
YipLau <sifooyee@gmail.com>
2018-09-05 18:12:26 +0800

    Transform: MyHello Demo

---
+++ Transforms/MyHello/MyHello.cpp
@@ -0,0 +1,27 @@
+
+#include "llvm/IR/Function.h"
+#include "llvm/Pass.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace llvm;
+
+#define DEBUG_TYPE "hello"
+
+namespace {
+  // Hello - The first implementation, without getAnalysisUsage.
+  struct MyHello : public FunctionPass {
+    static char ID; // Pass identification, replacement for typeid
+    MyHello() : FunctionPass(ID) {}
+
+    bool runOnFunction(Function &F) override {
+    	errs() << "Hello: ";
+    	errs().write_escaped(F.getName()) << '\n';
+    	return false;
+    }
+  };
+}
+
+char MyHello::ID = 0;
+static RegisterPass<MyHello> X("myhello", "Hello World Pass",
+                                false /* Only looks at CFG */,
+                                false /* Analysis Pass */);
