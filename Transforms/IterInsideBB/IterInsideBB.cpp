357371b9fec37e8561f28fa2a5845f50a2efb8e2
YipLau <sifooyee@gmail.com>
2018-09-06 15:43:55 +0800

    Transform: Update IterInsideBB

--- Transforms/IterInsideBB/IterInsideBB.cpp
+++ Transforms/IterInsideBB/IterInsideBB.cpp
@@ -1,5 +1,6 @@
 #include "llvm/IR/Function.h"
 #include "llvm/IR/InstIterator.h"
+#include "llvm/IR/Instruction.h"
 #include "llvm/Pass.h"
 #include "llvm/Support/raw_ostream.h"
 
@@ -15,17 +16,20 @@ namespace {
     bool runOnFunction(Function &Func) override {
         errs() << "Function name: " << Func.getName() << "\n";
         // ------------------------------------------------------------------------
-        for (Function::iterator BB=Func.begin(), E=Func.end(); BB!=E; ++BB) {
-            errs() << "Basic block name: " << BB->getName() << ", has "
-                << BB->size() << " instuctions.\n";
-            // for(BasicBlock::iterator IN=BB->begin(), EN=BB->end(); IN!=EN; ++IN) {
-            //     errs() << *IN << "\n";
-            // }
-        }
+        // for (Function::iterator BB=Func.begin(), E=Func.end(); BB!=E; ++BB) {
+        //     errs() << "Basic block name: " << BB->getName() << ", has "
+        //         << BB->size() << " instuctions.\n";
+        //     for(BasicBlock::iterator IN=BB->begin(), EN=BB->end(); IN!=EN; ++IN) {
+        //         errs() << *IN << "\n";
+        //     }
+        // }
         // ------------------------------------------------------------------------
-        for (BasicBlock &BasicBlock : Func) {
-            errs() << "Block name: " << BasicBlock.getName() << ", has "
-                << BasicBlock.size() << " instructions.\n";
+        for (BasicBlock &BB : Func) {
+            errs() << "Block name: " << BB.getName() << ", has "
+                << BB.size() << " instructions.\n";
+            for (Instruction &I : BB) {
+                errs() << I << "\n";
+            }
         }
         return false;
         }
